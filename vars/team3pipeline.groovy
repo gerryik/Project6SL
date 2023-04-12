d92a88b3b5d7275800f0b8232960bd2e87290e50
gerryik <33073924+gerryik@users.noreply.github.com>
2023-04-13 00:28:34 +0100

    Create team3pipeline.groovy

---
+++ vars/team3pipeline.groovy
@@ -0,0 +1,65 @@
+def call(String repoURL){
+  pipeline{
+  agent any
+  stages{
+    stage('project9'){
+      parallel{
+        stage('Gerald  Agbonye'){
+          when {
+                branch 'feature'
+            }
+          steps{
+            sh '/var/lib/jenkins/script/project9grp5q1scrpit.sh'
+          }
+        }
+        stage('Pretei Lemo'){
+          when {
+                branch 'feature'
+            }
+          steps{
+             sh '/var/lib/jenkins/script/lemorscript.sh'
+          }
+        }
+   stage('Odile Domingo'){
+    when {
+           branch 'feature'
+            }
+       steps{
+             sh '/var/lib/jenkins/script/odilescript.sh'
+        }
+    }
+  stage('Le Pere'){
+    when {
+           branch 'feature'
+            }
+       steps{
+            echo "TODO"
+        }
+    } 
+ stage('Henry Forson'){
+            when {      
+    branch 'feature'
+            }
+           steps {
+               echo "Other members not coming to group meetings"
+            }
+        }
+            stage("Other Members") {
+        when {
+                not {
+                    anyOf {
+                        branch 'master';
+                        branch 'develop';
+                        branch 'feature'
+                    }
+                }
+          }
+        steps{
+                echo "TODO"
+        }
+     }
+    }
+   }
+  }
+}
+}
